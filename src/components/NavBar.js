4d16a566dd72df4dbc38f39d1749f955ae549c9d
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-06-21 13:20:28 +0000

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+nav
 const NavBar = () => {
   return (
     <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-      <NavLink 
-        style={{ marginRight: '10px' }} 
+      <NavLink
+        style={{ marginRight: '10px' }}
         to="/"
       >
         Home
       </NavLink>
-      <NavLink 
-        style={{ marginRight: '10px' }} 
+      <NavLink
+        style={{ marginRight: '10px' }}
         to="/movies"
       >
         Movies
