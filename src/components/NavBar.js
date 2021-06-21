6654253d065ca5705c70f85228f2b74529a74f80
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-06-21 13:26:21 +0000

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-nav
+
 const NavBar = () => {
   return (
     <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
